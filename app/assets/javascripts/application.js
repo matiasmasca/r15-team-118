19bb34f45e33f01132359bc9a729517474250b6a
gabriel <gabriel.c1901@gmai.com>
2015-11-07 17:41:29 -0300

    added js for home#index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require easing
+//= require move-top
+//= require index
 //= require turbolinks
 //= require_tree .
